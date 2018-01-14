8c0617e1a04ac1fcc81997c9b9d3d797c9598482
Karen O <k.ortiz026@gmail.com>
2018-01-14 02:14:03 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
+
+var addElementToBeginningOfArray (array1,elem){
+  
+}
+
+var function destructivelyAddElementToBeginningOfArray(){
+  
+}
